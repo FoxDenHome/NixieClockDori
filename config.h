01a12658f06d8d2b8b9a889ed0ca982601b3ad0c
Mark Dietzer <mriq91@gmail.com>
2018-03-02 21:51:30 +0100

    fix effect speed

--- config.h
+++ config.h
@@ -1,7 +1,7 @@
 #define RENDER_USE_DELAY // Use delay when rendering instead of just returning for the next cycle
 //#define CLOCK_TRIM_HOURS
 
-#define EFFECT_SPEED 100
+#define EFFECT_SPEED 200
 #define EFFECT_SLOT_MACHINE
 
 #define ANTI_POISON_DELAY 200UL
