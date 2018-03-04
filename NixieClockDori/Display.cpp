efc1362f8cd8b0f84c6957585efec9164a730d06
Mark Dietzer <mriq91@gmail.com>
2018-03-04 01:24:39 +0100

    some free memory and color transitions

--- NixieClockDori/Display.cpp
+++ NixieClockDori/Display.cpp
@@ -65,7 +65,10 @@ bool insert2(const byte offset, const byte data, const bool trimLeadingZero, uin
 void renderNixies(Task *me) {
 #ifdef EFFECT_ENABLED
 	static unsigned long dataIsTransitioning[6] = { 0, 0, 0, 0, 0, 0 };
+	static boolean allTubesOld = true;
 	static uint16_t dataToDisplayOld[6] = { NO_TUBES, NO_TUBES, NO_TUBES, NO_TUBES, NO_TUBES, NO_TUBES };
+	static byte redOld, greenOld, blueOld, redPrevious, greenPrevious, bluePrevious;
+	static long colorTransProg;
 #endif
 
 	static byte anodeGroup = 0;
@@ -82,26 +85,59 @@ void renderNixies(Task *me) {
 		const uint16_t sym = getNumber((antiPoisonEnd - curMillis) / ANTI_POISON_DELAY);
 		tubeL = sym;
 		tubeR = sym;
-		analogWrite(PIN_LED_RED, 0);
-		analogWrite(PIN_LED_GREEN, 0);
-		analogWrite(PIN_LED_BLUE, 0);
 	}
 	else if (DisplayTask::current) {
-		if (DisplayTask::current->render(microDelta)) {
-			// Start necessary effects (when display changed)
+		const boolean allowEffects = DisplayTask::current->render(microDelta);
+
 #ifdef EFFECT_ENABLED
+		if (allowEffects) {
+			// Start necessary effects (when display changed)
 			for (byte i = 0; i < 6; i++) {
 				const uint16_t cur = DisplayTask::current->dataToDisplay[i];
-				if (dataToDisplayOld[i] != cur) {
+				if (dataToDisplayOld[i] != cur || allTubesOld) {
 					dataToDisplayOld[i] = cur;
 					dataIsTransitioning[i] = EFFECT_SPEED;
 				}
 			}
-#endif
+			allTubesOld = false;
+		} else {
+			allTubesOld = true;
 		}
+
+		if (DisplayTask::current->red != redOld) {
+			redPrevious = redOld;
+			redOld = DisplayTask::current->red;
+			colorTransProg = EFFECT_SPEED;
+		}
+		if (DisplayTask::current->green != greenOld) {
+			greenPrevious = greenOld;
+			greenOld = DisplayTask::current->green;
+			colorTransProg = EFFECT_SPEED;
+		}
+		if (DisplayTask::current->blue != blueOld) {
+			bluePrevious = blueOld;
+			blueOld = DisplayTask::current->blue;
+			colorTransProg = EFFECT_SPEED;
+		}
+
+		if (colorTransProg > 0 && allowEffects && colorTransProg > microDelta) {
+			colorTransProg -= microDelta;
+			analogWrite(PIN_LED_RED, redOld + (((redPrevious - redOld) * colorTransProg) / EFFECT_SPEED));
+			analogWrite(PIN_LED_GREEN, greenOld + (((greenPrevious - greenOld) * colorTransProg) / EFFECT_SPEED));
+			analogWrite(PIN_LED_BLUE, blueOld + (((bluePrevious - blueOld) * colorTransProg) / EFFECT_SPEED));
+		}
+		else if(colorTransProg >= 0) {
+			colorTransProg = -1;
+			analogWrite(PIN_LED_RED, redOld);
+			analogWrite(PIN_LED_GREEN, greenOld);
+			analogWrite(PIN_LED_BLUE, blueOld);
+		}
+#else
 		analogWrite(PIN_LED_RED, DisplayTask::current->red);
 		analogWrite(PIN_LED_GREEN, DisplayTask::current->green);
 		analogWrite(PIN_LED_BLUE, DisplayTask::current->blue);
+#endif
+
 		tubeL = DisplayTask::current->dataToDisplay[curTubeL];
 		tubeR = DisplayTask::current->dataToDisplay[curTubeR];
 		dotMask = DisplayTask::current->dotMask;
