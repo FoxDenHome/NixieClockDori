b4d0665338efb9afb68f6ccc9267e43cdf1a2d22
Mark Dietzer <mriq91@gmail.com>
2018-03-17 04:38:37 +0100

    fix

--- NixieClockDori/config.h
+++ NixieClockDori/config.h
@@ -10,7 +10,8 @@
 
 #ifndef FW_VERSION
 #ifdef GIT_COMMIT
-#define FW_ADD_QUOTES(s) #s
+#define FW_ADD_QUOTES_H(s) #s
+#define FW_ADD_QUOTES(s) FW_ADD_QUOTES_H(s)
 #define FW_VERSION FW_ADD_QUOTES(GIT_BRANCH) " " FW_ADD_QUOTES(GIT_COMMIT)
 #else
 #define FW_VERSION "unknown"
