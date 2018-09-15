01dbf4264a5d9e7d04cfbe920cc7c7042994a656
Mark Dietzer <mriq91@gmail.com>
2018-09-15 12:05:47 +0200

    move serial into flash task

--- NixieClockDori/DisplayTask_Flash.cpp
+++ NixieClockDori/DisplayTask_Flash.cpp
@@ -1,5 +1,48 @@
 #include "DisplayTask_Flash.h"
 #include "Display.h"
+#include "crcserial.h"
+
+static uint16_t charToTube(const byte chr) {
+	switch (chr) {
+		// Special tube layouts
+	case 'N':
+		return NO_TUBES;
+		break;
+	case 'A':
+		return ALL_TUBES;
+		break;
+
+		// Symbols for IN-19
+	case '%':
+		return SYMBOL_PERCENT;
+		break;
+	case 'M':
+		return SYMBOL_M;
+		break;
+	case 'P':
+		return SYMBOL_P;
+		break;
+	case 'm':
+		return SYMBOL_m;
+		break;
+	case 'K':
+		return SYMBOL_K;
+		break;
+	case 'n':
+		return SYMBOL_n;
+		break;
+	case 'u':
+		return SYMBOL_MICRO;
+		break;
+	case 'C':
+		return SYMBOL_DEGREES_C;
+		break;
+
+	default:
+		return getNumber(chr - '0');
+		break;
+	}
+}
 
 DisplayTask_Flash::DisplayTask_Flash() {
 	this->removeOnCantShow = true;
@@ -7,6 +50,23 @@ DisplayTask_Flash::DisplayTask_Flash() {
 
 bool DisplayTask_Flash::_canShow() const {
 	return this->endTime > 0 && millis() < this->endTime;
+}
+
+void DisplayTask_Flash::setDataFromSerial() {
+	const unsigned long curMillis = millis();
+
+	this->allowEffects = (curMillis - this->lastUpdate) >= 400;
+	this->lastUpdate = curMillis;
+	this->endTime = curMillis + (unsigned long)inputString.substring(1, 9).toInt();
+
+	this->dotMask = (inputString[9] - '0') | ((inputString[10] - '0') << 2) | ((inputString[11] - '0') << 4);
+
+	for (byte i = 0; i < 9; i++) {
+		this->setDisplayData(i, charToTube(inputString[i + 12]));
+	}
+
+	this->setColorFromInput(16, -1);
+	this->showIfPossibleOtherwiseRotateIfCurrent();
 }
 
 bool DisplayTask_Flash::refresh() {
