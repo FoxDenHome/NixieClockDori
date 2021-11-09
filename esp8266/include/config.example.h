a97a6f75058d63733484080542035c5e89cb8918
Doridian <git@doridian.net>
2021-11-08 16:23:22 -0800

    stuff

---
+++ esp8266/include/config.example.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#define WIFI_SSID "SSID"
+#define WIFI_PASSWORD "PASSWORD"
+
+// UTC - 8
+#define TIME_ZONE -8
+#define NTP_SERVER "192.168.4.10"
+
+#define OTA_PASSWORD "OTHER PASSWORD"
