27d24510c0a4db8ca3f13039277b68748ad720b8
Doridian <git@doridian.net>
2021-11-09 11:37:16 -0800

    Persist tz/ntpserver. it needs it

--- esp8266/src/main.cpp
+++ esp8266/src/main.cpp
@@ -24,6 +24,9 @@ void time_is_set(bool is_sntp){
   Serial.printf("^T%02d%02d%02d%02d%02d%02d$\n", now_tm.tm_hour, now_tm.tm_min, now_tm.tm_sec, now_tm.tm_mday, now_tm.tm_mon, now_tm.tm_year % 100);
 }
 
+char tz[EEPROM_LEN_STRING];
+char ntp_server[EEPROM_LEN_STRING];
+char hostname[EEPROM_LEN_STRING];
 void setup() {
   Serial.begin(115200);
   Serial.println(F("^EBooting...$"));
@@ -38,12 +41,16 @@ void setup() {
     Serial.println(F("^EConnected!$"));
   }
 
-  ArduinoOTA.setHostname(eepromRead(EEPROM_OTA_HOSTNAME).c_str());
+  strcpy(hostname, eepromRead(EEPROM_OTA_HOSTNAME).c_str());
+  ArduinoOTA.setHostname(hostname);
   ArduinoOTA.setPassword(eepromRead(EEPROM_OTA_PASSWORD).c_str());
   ArduinoOTA.begin();
 
   settimeofday_cb(time_is_set);
-  configTime(eepromRead(EEPROM_TIME_ZONE).c_str(), eepromRead(EEPROM_NTP_SERVER).c_str());
+
+  strcpy(tz, eepromRead(EEPROM_TIME_ZONE).c_str());
+  strcpy(ntp_server, eepromRead(EEPROM_NTP_SERVER).c_str());
+  configTime(tz, ntp_server);
 }
 
 void loop() {
